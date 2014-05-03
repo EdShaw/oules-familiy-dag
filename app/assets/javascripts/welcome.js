0e755b0960440d18eed9e61d8c10ad33e166acb8
Edward Shaw <edwardshaw9@gmail.com>
2014-05-03 20:31:34 +0100

    Zooming

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,8 @@
 function makeGraph(error, nodes, edges){
 
   var g = new dagre.Digraph();
+  var svg = d3.select("#dag-container");
+  var svg_group = d3.select("#dag");
 
   nodes.forEach(function(e,i,a){
     g.addNode(e.id, {label: e.name});
@@ -15,13 +17,21 @@ function makeGraph(error, nodes, edges){
   console.log(layout)
 
   var renderer = new dagreD3.Renderer();
-  renderer.run(g, d3.select("#dag"));
+  renderer.run(g, svg_group);
+
+  svg.call(d3.behavior.zoom().on("zoom", function() {
+    var ev = d3.event;
+    svg_group
+      .attr("transform", "translate(" + ev.translate + ") scale(" + ev.scale + ")");
+  }));
+
 }
 
 
 $(function(){
 
-  queue().defer(d3.json, '/people.json')
-       .defer(d3.json, '/parent_ofs.json')
-       .await(makeGraph);
+  queue()
+      .defer(d3.json, '/people.json')
+      .defer(d3.json, '/parent_ofs.json')
+      .await(makeGraph);
 });
